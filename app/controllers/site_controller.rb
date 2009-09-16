5811eeb2c3856476420c68bba77efed905dc905d
Sidney Burks <sid137@gmail.com>
2009-09-17 01:17:55 +0200

    added text, copied over images and renamed

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,11 @@
+class SiteController < ActionController::Base
+
+  def index; end
+  def skills; end
+  def languages;   end
+  def services;  end
+  def contact;  end
+  def news;  end
+  def legal; end
+
+end
