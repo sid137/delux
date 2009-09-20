c7785d6a88bb0e8a5505b817652d6c2bb075adde
Sidney Burks <sid137@gmail.com>
2009-09-20 16:05:15 +0200

    trying to blueprint

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,11 +1,3 @@
-class SiteController < ActionController::Base
-
-  def index; end
-  def skills; end
-  def languages;   end
-  def services;  end
-  def contact;  end
-  def news;  end
-  def legal; end
+class SiteController < ApplicationController
 
 end
