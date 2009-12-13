57512dd04382437a7a48c5a38792f9d7e8dbba5d
Sidney Burks <sid137@gmail.com>
2009-12-13 03:15:52 +0100

    startinng I18n procedures

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
  
-  map.root :controller => "site", :action => "index"
+  map.index '/:locale', :controller => "site", :action => "index"
+  map.root :controller => "site", :action => "index", :locale => 'fr'
 
-  map.skills '/skills', :controller => "site", :action => "skills"
-  map.languages '/languages', :controller => "site", :action => "languages"
-  map.services '/services', :controller => "site", :action => "services"
-  map.contact '/contact', :controller => "site", :action => "contact"
-  map.news '/news', :controller => "site", :action => "news"  
-  map.legal '/legal', :controller => "site", :action => "legal"
+  map.skills '/skills', :controller => "site", :action => "skills", :path_prefix => '/:locale'
+  map.languages '/languages', :controller => "site", :action => "languages", :path_prefix => '/:locale'
+  map.services '/services', :controller => "site", :action => "services", :path_prefix => '/:locale'
+  map.contact '/contact', :controller => "site", :action => "contact", :path_prefix => '/:locale'
+  map.news '/news', :controller => "site", :action => "news", :path_prefix => '/:locale'
+  map.legal '/legal', :controller => "site", :action => "legal", :path_prefix => '/:locale'
 
 end
